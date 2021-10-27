3d68cda5fcaa662b62d97b8340294422f7bbd990
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2021-10-27 14:46:51 -0600

    Adding CRUD opperations to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,48 @@
 const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    let workoutData = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ]);
+    res.json(workoutData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+  const updateWorkout = await Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body,
+    },
+  });
+  try {
+    res.status(200).json(updateWorkout);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post("/api/workouts", async ({ body }, res) => {
+  const newWorkout = await Workout.create(body);
+  try {
+    res.status(200).json(newWorkout);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    let workoutData = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ]).limit(7);
+    res.json(workoutData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
